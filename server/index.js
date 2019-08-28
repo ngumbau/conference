4fc41571f72c834b3710e4f5abfcbdb7404d15f7
Kristopher Kivutha <kkibwana@gmail.com>
2019-08-28 22:17:10 +0300

    created the routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
 const app = express();
+
+const routes = require('./routes');
+
+app.use('/', routes());
 
 app.listen(3000, () => {
     console.log("server is up and running on port 3000")
---
+++ server/routes/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+const speakersRoute = require('./speakers')
+const feedbackRoute = require('./feedback')
+const indexRoute = require('./index')
+
+module.exports = () => {
+
+    router.get('/', (req, res, next) => {
+        return res.send("Index");
+    });
+
+    router.use('/speakers', speakersRoute());
+
+    router.use('/feedback', feedbackRoute());
+
+    return router;
+};
