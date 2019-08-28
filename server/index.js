d41a3f850134fa4aec4b541b1f33bd518a4ae54b
Kristopher Kivutha <kkibwana@gmail.com>
2019-08-28 22:37:33 +0300

    serving static files - images, css, js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const routes = require('./routes');
 
+app.use(express.static('public'));
+app.get('/favicon.ico', (req, res, next) => {
+    return res.sendStatus(204);
+})
 app.use('/', routes());
 
 app.listen(3000, () => {
