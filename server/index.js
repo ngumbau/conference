90baf250a284ad2ece2b37872e51335c7a465010
Kristopher Kivutha <kkibwana@gmail.com>
2019-09-18 10:45:18 +0300

    added custom error page

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const createError = require("http-errors");
 const path = require("path");
 
 const app = express();
@@ -14,6 +15,19 @@ app.get('/favicon.ico', (req, res, next) => {
     return res.sendStatus(204);
 })
 app.use('/', routes());
+
+app.use((req, res, next) => {
+    return next(createError(404, "File not Found"))
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    const status = err.status || 500;
+    res.locals.status = status;
+    res.locals.err = err;
+    res.status(status);
+    return res.render('error');
+});
 
 app.listen(3000, () => {
     console.log("server is up and running on port 3000")
